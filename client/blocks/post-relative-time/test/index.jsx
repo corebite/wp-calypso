b78b4b74ed3eb3a84c604b7146438aa9aecff20e
Marin Atanasov <tyxla@abv.bg>
2016-09-16 12:58:47 -0700

    Components: Add tests for PostRelativeTime component

---
+++ client/blocks/post-relative-time/test/index.jsx
@@ -0,0 +1,68 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import moment from 'moment';
+
+/**
+ * Internal dependencies
+ */
+import { PostRelativeTime } from 'blocks/post-relative-time';
+
+describe( 'PostRelativeTime', () => {
+	it ( 'should use the modified date if the post status is draft', () => {
+		const post = {
+			status: 'draft',
+			modified: '2016-09-14T15:47:33-04:00',
+			date: '2016-09-13T15:47:33-04:00'
+		};
+
+		const wrapper = shallow(
+			<PostRelativeTime
+				post={ post }
+				moment={ moment }
+			/>
+		);
+
+		const text = wrapper.find( '.post-relative-time__text' ).text();
+		expect( text ).to.equal( moment( post.modified ).fromNow() );
+	} );
+
+	it ( 'should use the modified date if the post status is pending', () => {
+		const post = {
+			status: 'pending',
+			modified: '2016-09-14T15:47:33-04:00',
+			date: '2016-09-13T15:47:33-04:00'
+		};
+
+		const wrapper = shallow(
+			<PostRelativeTime
+				post={ post }
+				moment={ moment }
+			/>
+		);
+
+		const text = wrapper.find( '.post-relative-time__text' ).text();
+		expect( text ).to.equal( moment( post.modified ).fromNow() );
+	} );
+
+	it ( 'should use the modified date if the post status is not pending/draft', () => {
+		const post = {
+			status: 'publish',
+			modified: '2016-09-14T15:47:33-04:00',
+			date: '2016-09-13T15:47:33-04:00'
+		};
+
+		const wrapper = shallow(
+			<PostRelativeTime
+				post={ post }
+				moment={ moment }
+			/>
+		);
+
+		const text = wrapper.find( '.post-relative-time__text' ).text();
+		expect( text ).to.equal( moment( post.date ).fromNow() );
+	} );
+} );
